c19e9aad87bdac418b2c763b66ef9bce633452d9
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-29 18:32:44 +0530

    first to last.java

---
+++ first to last.java
@@ -0,0 +1,23 @@
+public class firsttolast {
+    public static void main(String[]args){
+        int first= 1;
+        int last= 10;
+        while(first<=last)  #   Using loop statment,when the first value is to be lass than or equal to last value)
+        {
+            System.out.println(first);
+            first=first+1;   # Incrementing the first value        }
+
+    }    
+}
+
+#Output
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
