4e0c53b7651d4c091f18c28513ead8cc3a496724
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-29 18:58:14 +0530

    breakdemo.java

---
+++ breakdemo.java
@@ -0,0 +1,15 @@
+public class breakdemo {
+    public static void main(String[]args){
+        int number=0;
+        while (number<10) 
+        {
+            number=number+1; # addiing the number
+            if(number==5) # when given number compares to the value 5
+            break; # breaks (stop) the loop
+        }
+        {
+           System.out.println(number);   
+        }
+    }
+}    
+
