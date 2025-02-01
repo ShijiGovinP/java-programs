d8009fa823622e3d0813cf501aee135925f0f9ba
ShijiGovinP <shijigovin2004@gmail.com>
2025-02-01 20:39:55 +0530

    sizes.java

---
+++ sizes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Size {
+
+	public static void main(String[] args) {
+		               //   To find out size of datatypes
+		System.out.println(Byte.SIZE);               //8 bits
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);          
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.SIZE);
+		System.out.println(Character.SIZE);         //16 bits
+		System.out.println(Long.SIZE);             //64 bits
+		
+	}
+
+}
