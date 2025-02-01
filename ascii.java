843be0edd1d0f6c2292ce7bc9f5223930490076b
ShijiGovinP <shijigovin2004@gmail.com>
2025-02-01 20:42:36 +0530

    ascii.java

---
+++ ascii.java
@@ -0,0 +1,13 @@
+public class Ascii  
+{  
+public static void main(String[] args)   
+{   
+char ch1 = 'a';  
+char ch2 = 'b';   
+int asciivalue1 = ch1;  // variable stores the int value   
+int asciivalue2 = ch2;  //variable stores the int value  
+System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+}  
+}  
+
