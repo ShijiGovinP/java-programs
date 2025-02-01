c494c4f8dc2f99cafd30526f86557e6cf0f8bffb
ShijiGovinP <shijigovin2004@gmail.com>
2025-02-01 19:43:02 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<=n;i++){ //outer loop for i=o,and to =n,soi<=n
+            for(int j=0;j<n;j++) //inner loop j is equals to 0 and j is less than n and increment
+            {
+                System.out.print("*");
+
+            }
+            System.out.print("\n");
+        }
+    }
+}
