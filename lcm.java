3666abba56b007a40298eed1f76f3a5ecc05d9d6
ShijiGovinP <shijigovin2004@gmail.com>
2025-02-05 07:12:24 +0530

    lcm.java

---
+++ lcm.java
@@ -0,0 +1,18 @@
+public class lcm {
+        public static void main(String[] args) {
+            int a=100;
+            int b=30;
+            int res= Math.max(a,b); ///max numbers
+            while(true)
+            {
+                if(ans%a==0 && ans%b==0) 
+                    break;
+                    res++; //increment factors
+          
+            }
+            System.out.println(ans);
+    
+        }
+    
+    }
+    
